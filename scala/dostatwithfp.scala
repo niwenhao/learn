abce011874615611a8d7c4fb9c9161bdeade1006
niwenhao <niwenhao@gmail.com>
2016-08-04 13:08:17 +0900

    up

--- scala/dostatwithfp.scala
+++ scala/dostatwithfp.scala
@@ -2,9 +2,8 @@ import scala.collection.JavaConverters._
 import java.io.File
 import scala.util.matching.Regex
 
-def makeProcessor[POST, PAR, IN, OUT] : (PAR, (PAR, IN) => OUT, (OUT) => POST) => IN => POST = (p, f, fp) => (i) => fp(f(p, i))
-
-
+def makeProcessor[PAR, IN, OUT] : (PAR, (PAR, IN) => OUT) => IN => OUT = (p, f) => (i) => f(p, i)
+def pipe[I, J, K]: (I => J, J =>K) => I => K = (l, r) => (i) => r(l(i))
 
 def makeSelectProcessor(pattern: Regex, path: String) = {
     val f = new File(path)
@@ -14,13 +13,11 @@ def makeSelectProcessor(pattern: Regex, path: String) = {
 
 def makeCharCount(multi: Int, list: Array[File]) = list map { s => multi * s.getName().length() }
 
-val process = makeProcessor("^\\w".r, makeSelectProcessor,
-                makeProcessor(2, makeCharCount, (i : Array[Int]) => i))
+val p1 = makeProcessor("^\\w".r, makeSelectProcessor)
+val p2 = pipe(p1, makeProcessor(2, makeCharCount))
 
-
-process("/")
-    
-val p1 = makeProcessor(2, makeCharCount, (i : Array[Int]) => i)
-val p2 = makeProcessor("^\\w".r, makeSelectProcessor, p1)
-
+p1("/")
 p2("/")
+
+val p = pipe(makeProcessor("^\\w".r, makeSelectProcessor),
+                makeProcessor(2, makeCharCount)
